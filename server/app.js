30de3ccf75940434ad9d634a9397f48e16713b17
soltisclan <zoltiz@gmail.com>
2015-10-05 16:20:41 -0400

    Switching control to app.js, light refactoring of server code.

--- server/app.js
+++ server/app.js
@@ -20,38 +20,30 @@ con.connect(function(err){
 
 var server = http.createServer(function (request, response) {
 
-    var html = '{';
-  
-    console.dir(request.param);
-
     var queryObject = url.parse(request.url,true).query;
-    console.log(queryObject);
+    console.log("Query: " + queryObject);
+    console.log("Params: " + JSON.stringify(request.param,null,"\t"));
 
-    if (queryObject['device'] == null) {
+    if (queryObject.device == null) {
 	con.query('SELECT * FROM cubestate limit 1000;',function(err, rows){
-		for(var i =0; i<rows.length; i++) {
-			html += "{'deviceID':'" + rows[i].deviceID + "','occupied':'" + rows[i].occupied + "','ts':' " +rows[i].ts + "'},\n";
-		}        
-  	      	html += '}';
 		response.writeHead(200, {'Content-Type': 'application/json'});
-        	response.end(html);
+        response.end(JSON.stringify(rows, null, "\t"));
 	});
     }
     else
     {
-	var device = queryObject['device'];
-	var state = queryObject['occupied']
+	var device = queryObject.device;
+	var state = queryObject.occupied;
 	console.log(device + ', ' + state);
 	con.query('INSERT INTO cubestate (deviceId, occupied, ts) VALUES (' + con.escape(device) + ',' + con.escape(state) + ', now());', function(err, result) {
 		console.log("Err: " + err + ", Res: " + result);
 	});
 	response.writeHead(200, {'Content-Type': 'application/json'});
-        response.end("{'response':'Success'}");
+    response.end("{'response':'Success'}");
     }
 });
 
-// Listen on port 8000, IP defaults to 127.0.0.1
-server.listen(8080);
+server.listen(process.env.PORT || 8080);
 
 // Put a friendly message on the terminal
-console.log("Server running at http://127.0.0.1:8080/");
+console.log("Server is running on port " + (process.env.PORT || 8080));
