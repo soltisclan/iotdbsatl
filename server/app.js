9d3118f4b743a45f2603851419c0607d628815bb
soltisclan <zoltiz@gmail.com>
2015-10-05 17:39:44 -0400

    Put the client back in /client folder, removed temporary client Node app

--- server/app.js
+++ server/app.js
@@ -5,7 +5,7 @@ var express = require('express'),
 
 var app = express();
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, '../client')));
 
 
 var con = mysql.createConnection({
